c551822255922c2c48c49480908d6244f0eb7a15
mosespace <kisakyemoses50@gmail.com>
2024-02-15 10:50:20 +0300

    feat: completed the navbar added small responsive navbar

---
+++ components/frontend/NavBar.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Link from "next/link";
+import { Heart, Search, ShoppingCart } from "lucide-react";
+
+const links = [
+  { slug: "shop-deals", title: "shop deals" },
+  { slug: "home-favorites", title: "home favorites" },
+  { slug: "fashion-finds", title: "fashion finds" },
+  { slug: "gift-guides", title: "gift guides" },
+  { slug: "registry", title: "registry" },
+];
+
+const companyName = "Geko";
+
+export default function NavBar() {
+  return (
+    <header className='border-b px-8 md:px-10 py-3'>
+      {/* upper navbar */}
+      <div className='w-full md:max-w-6xl md:mx-auto flex flex-col lg:flex-row item-start md:items-center md:justify-between'>
+        {/* logo and the search */}
+        <Link href='/'>
+          <h2 className='text-2xl font-bold uppercase'>{companyName}</h2>
+        </Link>
+
+        <div className='bg-gray-100 border border-transparent focus-within:border-blue-500 flex rounded-full h-9 items-center px-4 w-full lg:w-2/4 mt-3 md:mx-auto max-lg:mt-6'>
+          <Search className='mr-3 rotate-90' />
+          <input
+            type='email'
+            placeholder='Search...'
+            className='w-full outline-none bg-transparent text-gray-600 font-semibold text-sm'
+          />
+        </div>
+
+        {/* signIn, favorites and the cart */}
+        <div className='hidden lg:flex items-center gap-5'>
+          <Link href='/register' className='text-sm font-bold'>
+            sign in
+          </Link>
+          <Link href='/favorite'>
+            <Heart className='mr-3' />
+          </Link>
+          <Link href='/cart' className='relative flex items-center'>
+            <ShoppingCart />
+            <div className='rounded-full bg-blue-500 text-white p-1 text-xs absolute z-[230] -right-4 -top-2 font-bold'>
+              20
+            </div>
+          </Link>
+        </div>
+      </div>
+
+      {/* downer navbar */}
+      <div className='py-4 hidden mt-3 md:flex justify-center items-center'>
+        <ul
+          id='collapseMenu'
+          className='lg:flex max-lg:hidden max-lg:w-full lg:space-x-10 max-lg:space-y-3 lg:absolute lg:left-1/2 lg:-translate-x-1/2'
+        >
+          {links.map((link, index) => (
+            <li key={index} className='max-lg:border-b max-lg:py-2'>
+              <Link
+                href={link.slug}
+                className='hover:text-[#007bff] capitalize text-gray-600 font-bold text-[15px] block'
+              >
+                {link.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </header>
+  );
+}
