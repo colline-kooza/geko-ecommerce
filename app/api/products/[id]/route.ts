fdc8da8607c596707ffd48c4cbce9132bed5c192
mosespace <kisakyemoses50@gmail.com>
2024-02-17 15:35:24 +0300

    feat: initial commit

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -23,3 +23,25 @@ export async function GET(req: NextApiRequest, context: any) {
     return NextResponse.json(error, { status: 500 });
   }
 }
+
+export async function DELETE(req: NextApiRequest, context: any) {
+  const { params } = context;
+  const id = params.id;
+  // console.log(id);
+
+  try {
+    const product = await db.product.delete({
+      where: {
+        id,
+      },
+    });
+
+    console.log("Product deleted successfully", product);
+    return NextResponse.json(product, {
+      status: 200,
+    });
+  } catch (error) {
+    console.log("Failed to delete a product", error);
+    return NextResponse.json(error, { status: 500 });
+  }
+}
