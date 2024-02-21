8009b9737c8de9bfb33a370bce4552b292cace2a
mosespace <kisakyemoses50@gmail.com>
2024-02-21 23:10:14 +0300

    feat: worked on english

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -2,27 +2,26 @@ import db from "@/utils/db";
 import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET(request:any, { params: { id } }:any) {
- 
+export async function GET(request: any, { params: { id } }: any) {
+  // console.log(id);
   try {
-    const products = await db.product.findUnique({
+    const product = await db.product.findUnique({
       where: {
         id,
       },
     });
 
-    console.log("Products fetched back back successfully", products);
-    return NextResponse.json(products, {
+    console.log("Product fetched back back successfully", product);
+    return NextResponse.json(product, {
       status: 200,
     });
   } catch (error) {
-    console.log("Failed to fetch products", error);
+    console.log("Failed to fetch product", error);
     return NextResponse.json(error, { status: 500 });
   }
 }
 
-export async function DELETE(request:any, { params: { id } }:any) {
-
+export async function DELETE(request: any, { params: { id } }: any) {
   try {
     const product = await db.product.delete({
       where: {
