c19341d624186072bae93d342168cfcc88c3212f
kooza-colline <koozacollinz1@gmail.com>
2024-02-22 01:51:24 +0300

    removed components

--- components/CheckOutComponents/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Button({title,className}) {
-  return (
-    <button type="submit" className={`${className} text-white bg-black hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}>{title}</button>    
-  )
-}
