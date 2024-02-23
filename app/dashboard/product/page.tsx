3070e25b91d37d50613905e235a6590bbfe82382
kooza-colline <koozacollinz1@gmail.com>
2024-02-23 07:57:25 +0300

    added active class backend

--- app/dashboard/product/page.tsx
+++ app/dashboard/product/page.tsx
@@ -4,6 +4,7 @@ import getData from "@/utils/getData";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { BsPencilSquare } from "react-icons/bs";
 
 export default async function page() {
   const products = await getData("products");
@@ -108,22 +109,7 @@ export default async function page() {
                           href={`/dashboard/product/update/${product.id}`}
                           className='cursor-pointer text-xs font-bold sm:text-sm md:text-base text-green-600 '
                         >
-                          <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            width='19'
-                            height='19'
-                            viewBox='0 0 24 24'
-                            fill='none'
-                            stroke='currentColor'
-                            strokeWidth='2'
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
-                            className='lucide lucide-external-link'
-                          >
-                            <path d='M15 3h6v6' />
-                            <path d='M10 14 21 3' />
-                            <path d='M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6' />
-                          </svg>
+                          <BsPencilSquare size={20}/>
                         </Link>
                       </div>
                     </div>
