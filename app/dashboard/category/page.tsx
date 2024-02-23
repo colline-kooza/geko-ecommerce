e2cb04891cfd4566c2a8c6700afef75b75081ea6
kooza-colline <koozacollinz1@gmail.com>
2024-02-23 07:16:00 +0300

    added clerk

--- app/dashboard/category/page.tsx
+++ app/dashboard/category/page.tsx
@@ -2,6 +2,7 @@ import Delete from '@/components/DashboardComponents/DeleteBtn'
 import Head from '@/components/DashboardComponents/Head'
 import { TextInputs } from '@/components/DashboardComponents/TextInputs'
 import getData from '@/utils/getData'
+import { BsPencilSquare } from "react-icons/bs";
 import { ExternalLink, Trash2 } from 'lucide'
 import Link from 'next/link'
 import React from 'react'
@@ -73,7 +74,7 @@ export default async function page() {
                        <Delete id={cat.id} resourceName="category"/>
 
                         <Link href={`/dashboard/category/update/${cat.id}`} className="cursor-pointer text-xs font-bold sm:text-sm md:text-base text-green-600 ">
-            <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg>
+                        <BsPencilSquare size={20}/>
         </Link>
            
                     </div>
